86d833f84b2b372730606076da85dd734cb4cc5f
akhil723 <akhilshibu1725@gmail.com>
2022-11-21 12:54:01 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistraionComponent } from './registraion/registraion.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistraionComponent,
+    HomeComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ReactiveFormsModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/registraion/registraion.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-registraion',
+  templateUrl: './registraion.component.html',
+  styleUrls: ['./registraion.component.css']
+})
+export class RegistraionComponent implements OnInit {
+  withdrawForm = this.fb.group({
+    userdf: ['', [Validators.required, Validators.pattern('[a-zA-Z]+')]],
+    userdl: ['', [Validators.required, Validators.pattern('[a-zA-Z@#$0-9]+')]],
+    userda: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+    userdob: ['', [Validators.required, Validators.pattern('[]+')]],
+    userdp: ['', [Validators.required, Validators.pattern('[0-9]+')]],
+    
+    
+    
+  })
+
+  constructor(private router:Router,private fb: FormBuilder,) { }
+
+  ngOnInit(): void {
+  }
+  register(){
+    if(this.withdrawForm.valid)
+    this.router.navigateByUrl('/home')
+    
+    
+
+}
+}
